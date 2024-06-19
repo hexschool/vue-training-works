0200f2a3a3ae4d66f42e4179ec34b6927fc822d3
TzuHui <angel1996421@gmail.com>
2024-06-19 17:33:26 +0800

    test2

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -77,7 +77,7 @@
               target="_blank"
               class="btn btn-dark btn-sm rounded-0"
             >
-              現折 $5200
+              現折 $5,200
               <i class="fa fa-arrow-right" aria-hidden="true"></i>
             </a>
           </div>
