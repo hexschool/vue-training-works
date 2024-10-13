d5612995d0573b86a27a63386d0215ebc770c0d9
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-10-13 11:49:32 +0800

    hotfix: 修正判斷

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ echo ${osStatus} "${lgreen}=====================================================
 echo ${osStatus} " 目前遠端分支: ${originUrl} "
 echo ${osStatus} "==============================================================${norm}"
 
-sleep 1s
+# sleep 1s
 split=${originUrl:0:5}
 status='';
 if [ $split = 'https' ]
@@ -86,7 +86,7 @@ echo ${osStatus} " 將於五秒後建立 vue.config.js "
 echo ${osStatus} " ＊如要停止動作請按下 CRTL + C or Control + C"
 echo ${osStatus} "==============================================================${norm}"
 
-sleep 5s
+# sleep 5s
 
 touch $work_path
 
@@ -98,7 +98,7 @@ echo ${osStatus} " 詳細可見該檔案下註解。 "
 echo ${osStatus} " ＊如要停止動作請按下 CRTL + C or Control + C"
 echo ${osStatus} "==============================================================${norm}"
 
-sleep 5s
+# sleep 5s
 
 echo "// 該檔案生成的方式可以在 Vue Ui 中的「設定」->「Vue Cli」->「公開路徑」找到。
 module.exports = {
@@ -124,7 +124,7 @@ echo ${osStatus} " 5 秒後開始編譯 Vue Cli "
 echo ${osStatus} " ＊如要停止動作請按下 CRTL + C or Control + C"
 echo ${osStatus} "==============================================================${norm}"
 
-sleep 5s
+# sleep 5s
 npm run build
 
 echo
@@ -153,7 +153,7 @@ echo ${osStatus} "${yellow}=====================================================
 echo ${osStatus} " 5 秒後部署檔案到遠端分支 "
 echo ${osStatus} " ＊如要停止動作請按下 CRTL + C or Control + C"
 echo ${osStatus} "==============================================================${norm}"
-sleep 5s
+# sleep 5s
 git push -f $originUrl master:gh-pages
 
 echo
